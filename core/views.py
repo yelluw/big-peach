d76954cd34fc912ea013fe16222cb491096b7e9d
Yelluw <pr@yelluw.com>
2021-05-18 00:06:31 -0400

    member form works. working on adding events from meetup.com

--- core/views.py
+++ core/views.py
@@ -1,22 +1,33 @@
 import logging
-from datetime import timedelta
-from django.utils import timezone
-from django.views.generic import TemplateView
-from events.models import Event
+from uuid import uuid4
+from django.urls import reverse_lazy
+from django.views.generic import FormView
+from django.contrib.auth.models import User
+from members.forms import MemberForm
+from members.models import Member
+
 
 logger = logging.getLogger(__name__)
 
 
-class IndexView(TemplateView):
-    ''' Index Page '''
+class IndexView(FormView):
+    form_class = MemberForm
     template_name = 'index.html'
-
+    success_url = reverse_lazy('index')
+    
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
-
-        now = timezone.now()
-        four_weeks_from_now = now + timedelta(weeks=4)
-
-        context['events'] = Event.objects.filter(
-            published=True, end__gte=now, start__lte=four_weeks_from_now)
+        # show hide membership form
+        context['is_member'] = self.request.session.get('is_member', False) 
         return context
+
+    def form_valid(self, form):
+        user = User.objects.create(
+            email=form.cleaned_data['email'],
+            username=uuid4().hex
+        )
+        Member.objects.create(
+            user=user
+        )
+        self.request.session['is_member'] = True
+        return super().form_valid(form)
--- members/forms.py
+++ members/forms.py
@@ -1,13 +1,27 @@
 from django.forms import ModelForm
 from django import forms
 from django.contrib.auth.models import User
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit, Layout, Div
+from crispy_forms.bootstrap import FieldWithButtons, StrictButton
 
 
 class MemberForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.layout = Layout(
+            Div(
+                FieldWithButtons('email', StrictButton('Join', css_class='btn-success', type='submit')),
+            )
+        )
+        super(MemberForm, self).__init__(*args, **kwargs)
+
+
     class Meta:
         model = User
         fields = ('email',)
-
+        
     def clean_email(self):
         email = self.cleaned_data['email']
         user = User.objects.filter(email=email)
